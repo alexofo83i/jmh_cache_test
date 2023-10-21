564585326ba3a5e1a66832b0c9bb43463c08e354
Alexander Fedorov <fedorov@netcracker.com>
2023-10-21 12:06:01 +0400

    added for only one purpose: do not forget how to compile and execute

---
+++ jmh_cache_benchmark/runBenchmark.bat
@@ -0,0 +1,8 @@
+REM mvn clean install
+REM "%JAVA_HOME%"\bin\java -Xmx16g -Xms16g -XX:+UnlockCommercialFeatures  -XX:+FlightRecorder -XX:FlightRecorderOptions=defaultrecording=true,dumponexit=true  -cp .\target\benchmarks.jar com.netcracker.util.Main
+REM "%JAVA_HOME%"\bin\java  -jar .\target\benchmarks.jar
+
+cd jmh_cache_test\jmh_cache_benchmark
+del .\target\benchmarks.jar
+mvn clean install
+"%JAVA_HOME%"\bin\java -Xmx2g -Xms2g -XX:+UnlockCommercialFeatures  -XX:+FlightRecorder -XX:FlightRecorderOptions=defaultrecording=true,dumponexit=true  -jar .\target\benchmarks.jar com.fedorov.benchmarks.MyBenchmarkGetOnly
