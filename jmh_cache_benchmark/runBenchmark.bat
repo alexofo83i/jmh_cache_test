227339a5d565fe04134a0bad13cddd1e4c44a849
Alexander Fedorov <fedorov@netcracker.com>
2023-10-21 12:40:16 +0400

    let it be in history only

--- jmh_cache_benchmark/runBenchmark.bat
+++ jmh_cache_benchmark/runBenchmark.bat
@@ -1,5 +1,5 @@
 REM mvn clean install
-REM "%JAVA_HOME%"\bin\java -Xmx16g -Xms16g -XX:+UnlockCommercialFeatures  -XX:+FlightRecorder -XX:FlightRecorderOptions=defaultrecording=true,dumponexit=true  -cp .\target\benchmarks.jar com.netcracker.util.Main
+REM "%JAVA_HOME%"\bin\java -Xmx16g -Xms16g -XX:+UnlockCommercialFeatures  -XX:+FlightRecorder -XX:FlightRecorderOptions=defaultrecording=true,dumponexit=true  -cp .\target\benchmarks.jar com.fedorov.util.Main
 REM "%JAVA_HOME%"\bin\java  -jar .\target\benchmarks.jar
 
 cd jmh_cache_test\jmh_cache_benchmark
