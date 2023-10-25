9691665a0db45998cf2abbc0cb8ac5e5d7e02b94
Alexander Fedorov <fedorov@netcracker.com>
2023-10-25 11:08:51 +0400

    Move filling of cache instance from MyBenchmark into CacheFiller

--- jmh_cache_benchmark/src/main/java/com/fedorov/benchmarks/MyBenchmark.java
+++ jmh_cache_benchmark/src/main/java/com/fedorov/benchmarks/MyBenchmark.java
@@ -1,15 +1,15 @@
- package com.fedorov.benchmarks;
+package com.fedorov.benchmarks;
 
- import java.lang.reflect.InvocationTargetException;
- import java.lang.reflect.Method;
- import java.nio.charset.Charset;
- import java.util.ArrayList;
- import java.util.Random;
- 
- import com.fedorov.util.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Random;
+
+import com.fedorov.util.generic.CacheFiller;
 import com.fedorov.util.generic.ICache;
   
- public class MyBenchmark {
+public class MyBenchmark {
  
     public final int MAX_KEY_LENGTH    = 20;
     public final int MAX_EHCACHE_SIZE  = 10000;
@@ -21,45 +21,46 @@ import com.fedorov.util.generic.ICache;
   
     public final ThreadLocal<Random> threadRand =  ThreadLocal.<Random>withInitial(() ->{
         return new Random();
-     });
+    });
  
      
-     public void setUp(String cacheImplementation) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, ClassNotFoundException{
-         Class<String> clazz = (Class<String>) Class.forName(cacheImplementation);
-         Method medod = clazz.getMethod("getInstance");
-         
-         cache = (ICache<String>) medod.invoke(null);
- 
-         System.out.println("");
-         System.out.println( "setup is invoked for class " + cache.getClass().getSimpleName() );
- 
-         keys = new ArrayList<>(  MAX_CACHE_SIZE  );
-         Random rand = new Random();
-         for ( int i = MAX_CACHE_SIZE - 1; i>= 0; i--){
-             String keyString = generateRandomKey(rand);
-             if( i<MAX_EHCACHE_SIZE){
-                 cache.put( keyString, keyString);
-             }
-             keys.add(keyString);
-         }
-         System.out.println("keys : " + keys.size());
-     }
- 
-     public String generateRandomKey(Random rand){
-         byte[] keyArray = new byte[MAX_KEY_LENGTH];
-         rand.nextBytes(keyArray);
-         return new String(keyArray, Charset.forName("UTF-8"));
-     }
- 
-     public String getRandomKeyExisting(){
-         int keyIndex = threadRand.get().nextInt(MAX_EHCACHE_SIZE);
-         return keys.get(keyIndex);
-     }
- 
-     public String getRandomKeyNonExisting(){
-         int keyIndex = MAX_EHCACHE_SIZE + threadRand.get().nextInt(MAX_CACHE_SIZE - MAX_EHCACHE_SIZE);
-         return keys.get(keyIndex);
-     }
-     
+    public void setUp(String cacheImplementation) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, ClassNotFoundException{
+        Class<String> clazz = (Class<String>) Class.forName(cacheImplementation);
+        Method medod = clazz.getMethod("getInstance");
+        
+        cache = (ICache<String>) medod.invoke(null);
+
+        System.out.println("");
+        System.out.println( "setup is invoked for class " + cache.getClass().getSimpleName() );
+
+        keys = new ArrayList<>(  MAX_CACHE_SIZE  );
+        Random rand = new Random();
+        //  for ( int i = MAX_CACHE_SIZE - 1; i>= 0; i--){
+        //      String keyString = generateRandomKey(rand);
+        //      if( i<MAX_EHCACHE_SIZE){
+        //          cache.put( keyString, keyString);
+        //      }
+        //      keys.add(keyString);
+        //  }
+        CacheFiller.fillTheCache(cache, keys, rand, MAX_CACHE_SIZE, MAX_EHCACHE_SIZE, MAX_KEY_LENGTH);
+        System.out.println("keys : " + keys.size());
+    }
+
+    public String generateRandomKey(Random rand){
+        byte[] keyArray = new byte[MAX_KEY_LENGTH];
+        rand.nextBytes(keyArray);
+        return new String(keyArray, Charset.forName("UTF-8"));
+    }
+
+    public String getRandomKeyExisting(){
+        int keyIndex = threadRand.get().nextInt(MAX_EHCACHE_SIZE);
+        return keys.get(keyIndex);
+    }
+
+    public String getRandomKeyNonExisting(){
+        int keyIndex = MAX_EHCACHE_SIZE + threadRand.get().nextInt(MAX_CACHE_SIZE - MAX_EHCACHE_SIZE);
+        return keys.get(keyIndex);
+    }
+        
  }
  
---
+++ jmh_cache_benchmark/src/main/java/com/fedorov/util/generic/CacheFiller.java
@@ -0,0 +1,29 @@
+package com.fedorov.util.generic;
+
+import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Random;
+ 
+
+public final class CacheFiller {
+    
+    private CacheFiller(){}
+     
+    public static void fillTheCache(ICache<String> cache, ArrayList<String> keys,Random rand, int MAX_CACHE_SIZE, int MAX_EHCACHE_SIZE, int MAX_KEY_LENGTH) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, ClassNotFoundException{
+        for ( int i = MAX_CACHE_SIZE - 1; i>= 0; i--){
+            String keyString = generateRandomKey(rand,MAX_KEY_LENGTH);
+            if( i<MAX_EHCACHE_SIZE){
+                cache.put( keyString, keyString);
+            }
+            keys.add(keyString);
+        }
+        System.out.println("keys : " + keys.size());
+    }
+
+    private static String generateRandomKey(Random rand, int MAX_KEY_LENGTH){
+        byte[] keyArray = new byte[MAX_KEY_LENGTH];
+        rand.nextBytes(keyArray);
+        return new String(keyArray, StandardCharsets.UTF_8 /*Charset.forName("UTF-8")*/);
+    }
+}
